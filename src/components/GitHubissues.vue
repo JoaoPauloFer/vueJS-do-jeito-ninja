ab16af88b7b4a3bdede0b068f972eb59e2513c74
João Paulo Fer <joaopaulo.data@gmail.com>
2022-07-20 12:53:24 -0300

    docs: 📑  Implementado um loading para os links

--- src/components/GitHubissues.vue
+++ src/components/GitHubissues.vue
@@ -54,7 +54,10 @@
             <tr :v-if="!!issues.length && !loader.getIssues" 
                 v-for="issue in issues" 
                 :key="issue.number">
-                <td><a @click.prevent.stop="getIssue(issue.number)" href="">{{ issue.number }}</a></td>
+                <td>
+                    <a @click.prevent.stop="getIssue(issue)" href="">{{ issue.number }}</a>
+                    <img v-if="issue.is_loading" src="/static/loading.svg" alt="" height="20">
+                </td>
                 <td>{{ issue.title }}</td>
             </tr>
             <tr v-if="!!!issues.length && !loader.getIssues">
@@ -104,15 +107,15 @@
                 }
             },
              
-            getIssue(issueId) {
+            getIssue(issue) {
                 if(this.username && this.repository){    
-                    this.loader.getIssue = true;
-                    const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues/${issueId}`;
+                    this.$set(issue, 'is_loading', true);
+                    const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues/${issue.number}`;
 
                     axios.get(url).then((response) => {
                         this.selectedIssue = response.data;
                     }).finally(() => {
-                        this.loader.getIssue = false;
+                        this.$set(issue, 'is_loading', false);
                     });
                 }
             },
